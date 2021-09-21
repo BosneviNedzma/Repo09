8f172f3afe4a7d59479da509dd862a28caf509ec
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-22 00:48:29 +0200

    Izmjene

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,11 +1,14 @@
 // Only change code below this line
-   function myDoWhile() {
-       var myNumbers = 0;
-       do {
-           console.log(myNumbers);
-           myNumbers++;
-       } while (myNumbers <= 9);
-   }
-   console.log(myDoWhile());
-// Only chnage code above this line
+function myDoWhile(){
+    var myNumbers = ""; i = 0;
+    do {
+       myNumbers += i;
+    if ( i != 9) {
+        myNumbers += ", ";
+    } i ++;
+} while (i < 10);
+     return myNumbers;
+}
+console.log(myDoWhile());
+// Only change code above this line
 module.exports = myDoWhile;
